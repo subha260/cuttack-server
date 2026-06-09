0fdf5497a78bfff47c0d90cd3d423dff7d2ef611
subha260 <118972356+subha260@users.noreply.github.com>
2026-06-10 00:32:31 +0530

    Render port fix

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const app = express()
 
 app.use(cors())
 
-const server = app.listen(5000, () => {
-  console.log("Server Running On Port 5000")
-})
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server Running On Port ${PORT}`);
+});
 
 /* WebSocket Server */
 
